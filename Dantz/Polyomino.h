d13f3545edfe834473e515558ea87c70c6ddd8b9
Jack Grahl <jack.grahl@yahoo.co.uk>
2015-03-11 00:21:54 +0000

    Complete the interface of the Polyomino object.

--- Dantz/Polyomino.h
+++ Dantz/Polyomino.h
@@ -1,5 +1,13 @@
+#include <string>
+#include <utility>
+#include <vector>
+
 class Polyomino {
 public:
-	Polyomino(int w, int h, bool* squares[]);
+	Polyomino(int w, int h, bool* squares[], std::string name);
+	int width;
+	int height;
+	std::vector<std::pair<int, int >> Points(int direction);
+	std::string name;
 };
 
