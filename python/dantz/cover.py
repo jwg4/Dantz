9efcd3e07c787818537de258064e862d70a5d94f
Jack Grahl <jack@prismfp.com>
2018-07-24 16:30:57 +0100

    Make the function call correctly.

--- python/dantz/cover.py
+++ python/dantz/cover.py
@@ -1,6 +1,6 @@
 import os
 
-from ctypes import c_int, c_char_p, c_void_p, cdll, POINTER
+from ctypes import c_int, c_char_p, c_void_p, cdll, POINTER, c_bool
 
 
 def exact_cover(rows, names):
@@ -11,18 +11,18 @@ def exact_cover(rows, names):
     c_count = c_int(count)
 
     c_names = (c_char_p * count)()
-    c_rows = (POINTER(c_int) * count)()
+    c_rows = (POINTER(c_bool) * count)()
 
     for i in range(0, count):
         c_s = c_char_p(names[i])
         c_names[i] = c_s
 
-        c_row = (c_int * width)()
+        c_row = (c_bool * width)()
         for j in range(0, width):
             c_row[j] = rows[i][j]
 
         c_rows[i] = c_row 
 
-    c_dantz = cdll.LoadLibrary(os.path.abspath("dantz.so"))
+    c_dantz = cdll.LoadLibrary(os.path.abspath("src/dantz.so"))
     result = c_dantz.solve_exact_cover(c_rows, c_names, c_width, c_count)
     return result
--- python/src/dantz.cpp
+++ python/src/dantz.cpp
@@ -3,10 +3,11 @@
 #include "DataObject.h"
 #include "TableFactory.h"
 
-extern "C" void solve_exact_cover(bool**, std::vector<std::string>, int, int);
+extern "C" void solve_exact_cover(bool**, char**, int, int);
 
-void solve_exact_cover(bool** rows, std::vector<std::string> names, int width, int count) {
-    TableFactory* tf = new TableFactory(rows, names, width, count);
+void solve_exact_cover(bool** rows, char** names, int width, int count) {
+    std::vector<std::string> namev = {"a", "b", "c", "d", "e", "f", "g"};
+
+    TableFactory* tf = new TableFactory(rows, namev, width, count);
     TableHeader* th = tf->createTable();
-    th->search(0);
 }
