31d7c647f6b601d66577136652735a216c6fa2e2
Jack Grahl <jack@prismfp.com>
2018-07-24 12:37:52 +0100

    Declare the function extern C.

--- python/src/dantz.cpp
+++ python/src/dantz.cpp
@@ -3,6 +3,8 @@
 #include "DataObject.h"
 #include "TableFactory.h"
 
+extern "C" void solve_exact_cover(bool**, std::vector<std::string>, int, int);
+
 void solve_exact_cover(bool** rows, std::vector<std::string> names, int width, int count) {
     TableFactory* tf = new TableFactory(rows, names, width, count);
     TableHeader* th = tf->createTable();
