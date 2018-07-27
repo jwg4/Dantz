8ee64204cb74ad08c16e74204400b8176d271878
Jack Grahl <jack@prismfp.com>
2018-07-27 15:56:02 +0100

    Add another test - check that we dont depend on order of the inputs.

--- python/tests/test_exact_cover.py
+++ python/tests/test_exact_cover.py
@@ -15,3 +15,19 @@ def test_basic_example():
     result = exact_cover(data, names)
     assert len(result) == 1
     assert sorted(result[0]) == sorted(expected)
+
+
+def test_basic_example_reordered():
+    data = [
+        [ 1, 0, 0, 1, 0, 0, 1 ],
+        [ 0, 0, 1, 0, 1, 1, 0 ],
+        [ 0, 1, 1, 0, 0, 1, 0 ],
+        [ 1, 0, 0, 1, 0, 0, 0 ],
+        [ 0, 1, 0, 0, 0, 0, 1 ],
+        [ 0, 0, 0, 1, 1, 0, 1 ],
+    ]
+    names = ["0", "1", "2", "3", "4", "5", "6"]
+    expected = [['2', '4', '5'], ['0', '3'], ['1', '6']]
+    result = exact_cover(data, names)
+    assert len(result) == 1
+    assert sorted(result[0]) == sorted(expected)
