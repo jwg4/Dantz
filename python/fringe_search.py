f6af5419bb55bfe0bac2d88038b88c2f11dfca02
Jack Grahl <jack@prismfp.com>
2018-08-06 16:03:42 +0100

    Search for a fringe with missing square.

--- python/fringe_search.py
+++ python/fringe_search.py
@@ -1,10 +1,8 @@
 from dantz_search import t_tetromino_tiling
 
 
-WIDTH = 9
-HEIGHT = 11
-START_X = [0] * HEIGHT
-END_X = [WIDTH] * HEIGHT
+START_X = [2, 2, 1] + [0] * 10
+_END_X = [4, 3, 4, 3, 5, 5, 5, 5, 5, 6, 5, 6, 5]
 
 
 def generate_points(starts, ends):
@@ -18,6 +16,8 @@ def points_set(starts, ends):
 
 
 if __name__ == '__main__':
-    points = points_set(START_X, END_X)
-    result = t_tetromino_tiling(points, n_gaps=5)
-    print(result)
+    for N in range(0, 10):
+        END_X = [ e + N * 4 for e in _END_X ]
+        points = points_set(START_X, END_X)
+        result = t_tetromino_tiling(points, n_gaps=1)
+        print(N, result)
